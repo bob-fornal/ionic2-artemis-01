c0113b7f537afdeb569e6e1d3c753eceaaf5be4e
Bob Fornal <bob@fornal.org>
2016-10-16 03:13:14 -0400

    Stage 06 for Presentation

---
+++ Stage 06/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, ViewChild } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from 'ionic-native';
+
+import { HomePage } from './pages/home/home';
+
+import { settings } from './settings/settings.class';
+import { DataService } from './services/data-service';
+
+@Component({
+  templateUrl: './app.component.html'
+})
+export class ArtemisSchedulerApp {
+
+  rootPage = HomePage;
+
+  constructor(
+    dataService: DataService,
+    platform: Platform
+  ) {
+    platform.ready().then(() => {
+      StatusBar.styleDefault();
+
+      dataService.getFeatureToggles().subscribe(
+        (toggles) => {
+          settings.IsDebug = toggles.IsDebug;
+          settings.features = toggles.features;
+          (settings.IsDebug) && console.log("app.component - getFeatureToggles", toggles);
+        }
+      );
+    });
+  }
+
+}
